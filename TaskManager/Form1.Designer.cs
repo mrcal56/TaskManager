33c8cc1519e180f87b026c4dc3f590752acd7f9c
mrcal56 <marlon56r@gmail.com>
2024-08-09 21:56:08 -0600

    Agregar archivos de proyecto.

---
+++ TaskManager/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.maximizeButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanelTasks = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtTask.Location = new System.Drawing.Point(377, 78);
+            this.txtTask.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(132, 22);
+            this.txtTask.TabIndex = 1;
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAddTask.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnAddTask.Location = new System.Drawing.Point(376, 116);
+            this.btnAddTask.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(133, 27);
+            this.btnAddTask.TabIndex = 2;
+            this.btnAddTask.Text = "Añadir Tarea";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(354, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Escribe la tarea a guardar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.minimizeButton);
+            this.panel1.Controls.Add(this.maximizeButton);
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(881, 25);
+            this.panel1.TabIndex = 5;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Task Manager";
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Image = global::TaskManager.Properties.Resources.Minimize;
+            this.minimizeButton.Location = new System.Drawing.Point(771, 0);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(35, 25);
+            this.minimizeButton.TabIndex = 8;
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // maximizeButton
+            // 
+            this.maximizeButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.maximizeButton.FlatAppearance.BorderSize = 0;
+            this.maximizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.maximizeButton.Image = global::TaskManager.Properties.Resources.Fullscreen;
+            this.maximizeButton.Location = new System.Drawing.Point(806, 0);
+            this.maximizeButton.Name = "maximizeButton";
+            this.maximizeButton.Size = new System.Drawing.Size(35, 25);
+            this.maximizeButton.TabIndex = 7;
+            this.maximizeButton.UseVisualStyleBackColor = true;
+            this.maximizeButton.Click += new System.EventHandler(this.maximizeButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.exitButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Image = global::TaskManager.Properties.Resources.Close;
+            this.exitButton.Location = new System.Drawing.Point(841, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(40, 25);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // flowLayoutPanelTasks
+            // 
+            this.flowLayoutPanelTasks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanelTasks.AutoScroll = true;
+            this.flowLayoutPanelTasks.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.flowLayoutPanelTasks.Location = new System.Drawing.Point(83, 204);
+            this.flowLayoutPanelTasks.Name = "flowLayoutPanelTasks";
+            this.flowLayoutPanelTasks.Size = new System.Drawing.Size(723, 174);
+            this.flowLayoutPanelTasks.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(881, 519);
+            this.Controls.Add(this.flowLayoutPanelTasks);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.txtTask);
+            this.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(100, 200);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.Button maximizeButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelTasks;
+    }
+}
+
---
+++ TaskManager/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskManager.Properties;
+
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        private List<string> tasks = new List<string>();
+        private const string filePath = "tasks.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr one, int two, int three, int four);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            string task = txtTask.Text;
+            if (!string.IsNullOrWhiteSpace(task))
+            {
+                tasks.Add(task);
+                UpdateTaskList();
+                SaveTasks();
+                txtTask.Clear();
+            }
+        }
+
+        
+
+        private void UpdateTaskList()
+        {
+            flowLayoutPanelTasks.Controls.Clear();
+            foreach (var task in tasks)
+            {
+                var item = new ListViewItem();
+                AddTaskToPanel(task);
+            }
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(filePath))
+            {
+                tasks = File.ReadAllLines(filePath).ToList();
+                UpdateTaskList();
+            }
+        }
+
+        private void SaveTasks()
+        {
+            File.WriteAllLines(filePath, tasks);
+
+        }
+
+        private void AddTaskToPanel(string task)
+        {
+            Panel taskPanel = new Panel
+            {
+                 Width = flowLayoutPanelTasks.Width - 25,
+                 Height = 30,
+                 BorderStyle = BorderStyle.None
+            };
+
+            CheckBox checkBox = new CheckBox 
+            { 
+                Location = new Point(5,5),
+                Width = 20, 
+                Height = 20
+            };
+
+            checkBox.CheckedChanged += (s, e) => 
+            {
+                taskPanel.BackColor = checkBox.Checked ? Color.LightGreen : Color.White;
+            };
+
+            Label label = new Label
+            { 
+                Text = task,
+                Location = new Point(30, 5),
+                Width = 400, Height = 20 
+            };
+
+            Button deleteButton = new Button 
+            { 
+                Text = "Borrar",
+                Location = new Point(taskPanel.Width - 75, 2),
+                Width = 60,
+                Height = 25 
+            };
+
+            deleteButton.Click += (s, e) => 
+            {
+                tasks.Remove(task);
+                flowLayoutPanelTasks.Controls.Remove(taskPanel);
+                SaveTasks();
+            };
+
+            taskPanel.Controls.Add(checkBox);
+            taskPanel.Controls.Add(label);
+            taskPanel.Controls.Add(deleteButton);
+
+            flowLayoutPanelTasks.Controls.Add(taskPanel);
+
+        }
+        
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, 0x112, 0xf012, 0);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void maximizeButton_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+                maximizeButton.Image = Resources.NormalScreen;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                maximizeButton.Image = Resources.NormalScreen;
+            }
+        }
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        
+
+        
+    }
+}
